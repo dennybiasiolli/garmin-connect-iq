fe498604f33c14d158d243c547f46d335af15300
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-09-17 17:22:58 +0200

    updating README, adding compile-batch.sh file

--- README.md
+++ README.md
@@ -13,3 +13,36 @@ using [Garmin Express](https://www.garmin.com/en-US/software/express).
 
 Follow [Programmer's Guide](https://developer.garmin.com/connect-iq/programmers-guide/getting-started/)
 to setup your Windows or Mac.
+
+
+## Upload apps on Connect IQ Store
+
+https://apps.garmin.com/en-US/developer/dashboard
+
+
+## Batch compiling via command line
+
+`sh compile-batch.sh`
+
+
+## Command line guide
+
+```sh
+# Launch the simulator:
+connectiq
+
+# Compile the executable:
+monkeyc -d fenix6pro \
+    -f KeepCalmAndTriathlon/monkey.jungle \
+    -o KeepCalmAndTriathlon.prg \
+    -y $HOME/Downloads/developer_key.der
+
+# Run in the simulator
+monkeydo KeepCalmAndTriathlon.prg fenix6pro
+
+# Compile for Publishing
+monkeyc -e \
+    -o KeepCalmAndTriathlon.iq \
+    -w -f KeepCalmAndTriathlon/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+```
---
+++ compile-batch.sh
@@ -0,0 +1,28 @@
+monkeyc -e \
+    -o Analog24hour.iq \
+    -w -f Analog24hour/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+monkeyc -e \
+    -o AnalogDigital.iq \
+    -w -f AnalogDigital/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+monkeyc -e \
+    -o KeepCalmAndDuathlon.iq \
+    -w -f KeepCalmAndDuathlon/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+monkeyc -e \
+    -o KeepCalmAndRideBike.iq \
+    -w -f KeepCalmAndRideBike/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+monkeyc -e \
+    -o KeepCalmAndRun.iq \
+    -w -f KeepCalmAndRun/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+monkeyc -e \
+    -o KeepCalmAndSwim.iq \
+    -w -f KeepCalmAndSwim/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
+monkeyc -e \
+    -o KeepCalmAndTriathlon.iq \
+    -w -f KeepCalmAndTriathlon/monkey.jungle \
+    -y $HOME/Downloads/developer_key.der
